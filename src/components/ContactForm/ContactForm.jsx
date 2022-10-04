00a1a4b12f27adc6c0270a9a2c382029359cf696
VladaDumbadze <87700517+VladaDumbadze@users.noreply.github.com>
2022-10-04 16:26:19 +0300

    inputupdated

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,34 +1,35 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { PatternFormat } from 'react-number-format';
 import PropTypes from 'prop-types';
 import shortid from 'shortid';
 
 class ContactForm extends Component {
-    state = {
-        name: '',
-        number: ''
-    };
+  state = {
+    name: '',
+    number: '',
+  };
   formNameId = shortid.generate();
   formNumberId = shortid.generate();
-    handleChange = e => {
+  handleChange = e => {
     const { name, value } = e.currentTarget;
-    this.setState({ [name] : value });
-    };
+    this.setState({ [name]: value });
+  };
 
-    hendleSubmit = e => {
-      e.preventDefault();
-      this.props.onSubmit(this.state)
-      this.reset();
-    };
+  hendleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.reset();
+  };
 
   reset = () => {
     this.setState({ name: '', number: '' });
-   }
-    render() {
-        return (
-            <form onSubmit={this.hendleSubmit} >
+  };
+  render() {
+    return (
+      <form onSubmit={this.hendleSubmit}>
         <label htmlFor={this.formNameId}>
           Name
-            <input
+          <input
             id={this.formNameId}
             type="text"
             name="name"
@@ -36,13 +37,13 @@ class ContactForm extends Component {
             value={this.state.name}
             onChange={this.handleChange}
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
         </label>
         <label htmlFor={this.formNumberId}>
-          Number 
-           <input
+          Number
+          {/* <input
             id={this.formNumberId}
             type="tel"
             name="number"
@@ -53,18 +54,27 @@ class ContactForm extends Component {
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
               
-            />
+              /> */}
+          <PatternFormat
+            value={this.state.number}
+            id={this.formNumberId}
+            name="number"
+            format="+38 (###) ### ## ##"
+            allowEmptyFormatting
+            mask="_"
+            onChange={this.handleChange}
+            required
+          />
         </label>
 
         <button type="submit">Add contact</button>
       </form>
-        );
-    }
+    );
+  }
 }
 
 export default ContactForm;
 
 ContactForm.propTypes = {
-  
   onSubmit: PropTypes.func.isRequired,
 };
